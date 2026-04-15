747812d35fce8984ece84d7e340aa45eb64ce32f
TwoSheep-L <1249611204@qq.com>
2026-04-15 20:48:51 +0800

    feat: 注册工具到main

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,9 +1,13 @@
 import FsTool from "./FsTools.js";
+import ImageGeneraterTool from "./ImageGeneraterTool.js";
 import { toolRegistery } from "./registery.js";
 import RunCommand from "./RunCommandTool.js";
 import UserInputTool from "./user_input.js";
+import VideoGeneraterTool from "./VideoGeneraterTool.js";
 
 // 注册工具
 toolRegistery.registerTool(new UserInputTool()); // 请求用户输入
 toolRegistery.registerTool(new FsTool()); //  文件操作工具
 toolRegistery.registerTool(new RunCommand()); // 执行命令行
+toolRegistery.registerTool(new ImageGeneraterTool()); // 图片生成工具
+toolRegistery.registerTool(new VideoGeneraterTool()); // 连续视频生成工具
